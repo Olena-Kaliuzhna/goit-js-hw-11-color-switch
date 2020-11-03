3aae019fa71ec196ee7a571d4e3ce0e78f9da2ea
Olena Kaliuzhna <om.kaliuzhna.dz@gmail.com>
2020-11-03 13:02:29 +0200

    Initial commit

---
+++ src/js/color-switch.js
@@ -0,0 +1,40 @@
+import colors from './colors-list.js';
+
+const bodyEl = document.querySelector('body');
+const btnStartEl = document.querySelector('[data-action="start"]');
+const btnStopEl = document.querySelector('[data-action="stop"]');
+
+let interval = null;
+let isActive = false;
+
+btnStartEl.addEventListener('click', onBtnStarClick);
+btnStopEl.addEventListener('click', onBtnStopClick);
+
+function onBtnStarClick() {
+    if (isActive) {
+    return;
+  };
+  isActive = true;
+  interval = setInterval(switchColor, 1000);
+  
+};
+
+function onBtnStopClick() {
+  clearInterval(interval);
+  isActive = false;
+ 
+};
+
+
+
+function switchColor() { 
+  const randomIntegerFromInterval = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  };
+
+  let colorsInd = randomIntegerFromInterval(0, colors.length - 1);
+  
+  bodyEl.style.backgroundColor = colors[colorsInd];
+ 
+};
+
